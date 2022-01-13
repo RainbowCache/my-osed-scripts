fbaf1cab6d66b1a38c1a86f25a1450d5171e9a60
NyanNyanKoneko <statickate@gmail.com>
2022-01-13 10:47:34 -0800

    Added shebangs to a bunch of files.

--- 4_shellcode/2_find_function.py
+++ 4_shellcode/2_find_function.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import ctypes, struct
 from keystone import *
 
---
+++ 5_reverse_engineering/1_send_data.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import socket
+import sys
+
+buf = bytearray([0x41] * 100)
+
+
+def main():
+    if len(sys.argv) != 2:
+        server = "192.168.185.10"
+    else:
+        server = sys.argv[1]
+
+    port = 11460
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((server, port))
+
+    s.send(buf)
+    s.close()
+
+    print("[+] Packet sent")
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
